f8abcaeb9a67e51c2f243ea1e14d9367d20fcad6
Vagrant Cascadian <vagrant@debian.org>
2017-10-09 23:14:03 +0100

    omap5_distro_bootcmd
    ===================================================================
    
    Gbp-Pq: Topic am57xx
    Gbp-Pq: Name omap5_distro_bootcmd

--- include/configs/ti_omap5_common.h
+++ include/configs/ti_omap5_common.h
@@ -58,6 +58,21 @@
 #include <environment/ti/boot.h>
 #include <environment/ti/mmc.h>
 
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0) \
+	func(MMC, mmc, 1) \
+	func(PXE, pxe, na) \
+	func(DHCP, dhcp, na)
+
+#ifdef CONFIG_BOOTCOMMAND
+#undef CONFIG_BOOTCOMMAND
+#endif
+#define CONFIG_BOOTCOMMAND \
+	"run findfdt; " \
+	"run distro_bootcmd"
+
+#include <config_distro_bootcmd.h>
+
 #define CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	DEFAULT_LINUX_BOOT_ENV \
@@ -67,6 +82,7 @@
 	DEFAULT_FDT_TI_ARGS \
 	DFUARGS \
 	NETARGS \
+	BOOTENV \
 
 /*
  * SPL related defines.  The Public RAM memory map the ROM defines the
