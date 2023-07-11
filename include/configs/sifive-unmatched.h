87172324ffc73b1a5912432df59c6affd8172841
Vagrant Cascadian <vagrant@debian.org>
2023-07-11 03:54:17 +0100

    prevent relocating initrd & fdt, that results in failure to boot
    Bug-Ubuntu: https://bugs.launchpad.net/ubuntu/+source/u-boot/+bug/1937246
    
    ===================================================================
    
    Gbp-Pq: Topic riscv64
    Gbp-Pq: Name unmatched-prevent-relocating-initrd-and-fdt.patch

--- include/configs/sifive-unmatched.h
+++ include/configs/sifive-unmatched.h
@@ -36,6 +36,8 @@
 	"name=system,size=-,bootable,type=${type_guid_gpt_system};"
 
 #define CFG_EXTRA_ENV_SETTINGS \
+	"fdt_high=0xffffffffffffffff\0" \
+	"initrd_high=0xffffffffffffffff\0" \
 	"kernel_addr_r=0x84000000\0" \
 	"kernel_comp_addr_r=0x88000000\0" \
 	"kernel_comp_size=0x4000000\0" \
